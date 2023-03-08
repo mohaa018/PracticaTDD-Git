35d9689199ef1b6adfdc0558aea94564619897a3
mohaa018 <moha2020be@gmail.com>
2023-03-08 19:31:24 +0100

    Testear el 3 ... corregir el método ... Testear de nuevo

---
+++ Tareas-TDD/Tests/FizzBuzzTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testdevuelvenumerosenformatocadena(){
+        //Arrange
+        FizzBuzz test1 = new FizzBuzz();
+        FizzBuzz test2 = new FizzBuzz();
+
+        //Act
+        String resultado1 = test1.calcularfizzbuzz(1);
+        String resultado2 = test2.calcularfizzbuzz(2);
+
+        //Assert
+        assertEquals("1",resultado1 );
+        assertEquals("2", resultado2);
+
+    }
+
+    @Test
+    public void testcomprobarel3(){
+        //Arrange
+        FizzBuzz test3 = new FizzBuzz();
+
+        //Act
+        String resultado3 = test3.calcularfizzbuzz(3);
+
+        //Assert
+        assertEquals("Fizz",resultado3 );
+
+    }
+
+}
---
+++ Tareas-TDD/src/FizzBuzz.java
@@ -0,0 +1,15 @@
+import java.awt.geom.RectangularShape;
+
+public class FizzBuzz {
+
+    public String calcularfizzbuzz(int num){
+        if (num%3==0){
+
+            return "Fizz";
+        } else {
+            String solucion = String.valueOf(num);
+            return solucion;
+        }
+
+    }
+}
